b15695d6a155033691164b03175edc0f2f6a8f87
Billy Baney <bbaney0315@gmail.com>
2024-02-13 09:37:44 -0600

    finished Fibonacci Sequence problem

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    // Step 1. Make sure argument is a number
+    let number = Number(num)
+    // Step 2. Make sure number is positive
+    if (number < 0) return "OOPS"
+    // Step 3. Make sure number can be 0
+    if (number === 0) return 0
+    // Step 5. Use recursion to create fibonacci sequence
+    return number <= 2 ? 1 : fibonacci(number - 1) + fibonacci(number - 2)
+
 
 };
+
+console.log(fibonacci(5))
 
 // Do not edit below this line
 module.exports = fibonacci;
