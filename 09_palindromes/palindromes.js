14c870c5f4771cc28d2045e76d92ae4b8c9859a4
Billy Baney <bbaney0315@gmail.com>
2024-02-13 09:18:48 -0600

    finished 09-Palindromes problem

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let lowerCaseWord = word.toLowerCase()
+    let punctuation = /[\.,?!]/g;
+    let noPunc = lowerCaseWord.replace(punctuation, "")
+    let arr = noPunc.split('')
+    let solution = []
 
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== " ") {
+            solution.push(arr[i])
+        }
+    }
+
+    return solution.join("") === solution.reverse().join("") ? true : false
 };
+
+// console.log(palindromes("A car, a man, a maraca."))
 
 // Do not edit below this line
 module.exports = palindromes;
