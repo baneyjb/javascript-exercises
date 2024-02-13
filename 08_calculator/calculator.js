dfd2b1e2a5e00d7d60987ad1b75e5c075874df37
Billy Baney <bbaney0315@gmail.com>
2024-02-13 08:57:18 -0600

    finished 08-Calculator problems

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if (arr.length === 0) return 0
+  if (arr.length === 1) return arr[0]
+  return arr.reduce((acc, current) => acc + current)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((acc, current) => acc * current)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1
+	let total = 1
+  for (let i = 1; i <= num; i++) {
+    if (i <= num) {
+      total *= i
+    }
+  }
+  return total
 };
 
 // Do not edit below this line
